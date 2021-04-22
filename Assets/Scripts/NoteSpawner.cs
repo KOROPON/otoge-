f31edd8bc9360f92fe2fe3547db2ff9c68ecb250
KOROPON <koropon3141@gmail.com>
2021-04-22 17:51:03 +0900

    タップノーツを組み込んでみました

--- Assets/Scripts/NoteSpawner.cs
+++ Assets/Scripts/NoteSpawner.cs
@@ -12,10 +12,10 @@ public class NoteSpawner : MonoBehaviour {
   void Start() {
     song = JsonUtility.FromJson<Song>(songFile.text);
     foreach (Tap tap in song.taps) {
-      Instantiate(tapPrefab, new Vector3(tap.channel, 0, tap.start), Quaternion.identity);
+      Instantiate(tapPrefab, new Vector3(tap.channel + 4, -0.5f, tap.start), Quaternion.identity);
     }
     foreach (Hold hold in song.holds) {
-      GameObject obj = Instantiate(holdPrefab, new Vector3(hold.channel, 0, hold.start), Quaternion.identity);
+      GameObject obj = Instantiate(holdPrefab, new Vector3(hold.channel + 4, -0.5f, hold.start), Quaternion.identity);
       obj.transform.localScale = new Vector3(1, 1, hold.end - hold.start);
     }
   }
