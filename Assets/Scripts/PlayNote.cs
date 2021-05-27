3a68361e593965384bc00b6f4f2dca299fafec7b
KOROPON <koropon3141@gmail.com>
2021-05-27 17:03:58 +0900

    秒数でノーツを動かせました。なおバグ

--- Assets/Scripts/PlayNote.cs
+++ Assets/Scripts/PlayNote.cs
@@ -5,20 +5,12 @@ using UnityEngine;
 
 public class PlayNote : MonoBehaviour {
     public float spd;
+    
     void Start()
     {
-        spd = GameObject.Find("Run Programs").GetComponent<Variable>().speed;
+      spd = Variable.speed;
     }
-    //void Main() {
-    //var timer = new Timer(30);
-    // ^C}[Ì
-    //timer.Elapsed += (sender, e) => {
-    //Debug.Log("");
-    //Vector3 pos = this.gameObject.transform.position;
-    //this.gameObject.transform.position = new Vector3(pos.x, pos.y, pos.z + spd / 10);
-    //};
-    //timer.Start();
-    //}
+
     void Update() {
     Vector3 pos = this.gameObject.transform.position;
     this.gameObject.transform.position = new Vector3(pos.x, pos.y, pos.z + spd);
