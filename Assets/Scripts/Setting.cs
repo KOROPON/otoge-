41f588314b51f430e77cbc9fef8f85a35ef7bf83
Outdoor <shoutarou8888ha@gmail.com>
2021-06-02 16:37:13 +0900

    設定画面修正

--- Assets/Scripts/Setting.cs
+++ Assets/Scripts/Setting.cs
@@ -1,16 +1,16 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 
 public class Setting : MonoBehaviour
 {
   //下準備
   string rate="";
-
   public void RateChange(double change_value){
     GameObject.Find("rate").GetComponent<Text>().text=(double.Parse(GameObject.Find("rate").GetComponent<Text>().text)+change_value).ToString();
   }
+
   [SerializeField] GameObject alter;
   public void SettingOpen(){
     alter.SetActive(true);
@@ -24,6 +24,7 @@ public class Setting : MonoBehaviour
   }
   public void cancel(){
     GameObject.Find("rate").GetComponent<Text>().text=rate;
+    GameObject.Find("設定画面").SetActive(false);
   }
   public void Up(){
     RateChange(0.1);
