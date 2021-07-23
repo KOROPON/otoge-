fb3025f5514ce25bbf6ad7c4f32bc50f8e4fbf33
Isigater <sige03830@gmail.com>
2021-07-24 02:00:14 +0900

    曲選択画面に関するいろいろ
    まずマージで消えたスクリプトの再現
    それに伴ってファイルの名前の変更(表記の統一化)
    Songsファイルの位置を　Project/Assets/Songs から Project/Assets/Resources/Songs　へ移動

--- Assets/Scripts/Select Scene/MusicNumManage.cs
+++ Assets/Scripts/Select Scene/MusicNumManage.cs
@@ -1,29 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MusicNumManage : MonoBehaviour
 {
     RawImage jack;
     public Text text1;
     public AudioSource audioSource;
-    public int music_number;
+    public int music_number = -1;
     public AudioClip collide;
     public AudioClip devou;
 
     void Start()
     {
         jack = GameObject.Find("ジャケット1").GetComponent<RawImage>();
+        audioSource = GameObject.Find("Audio Source Intro").GetComponent<AudioSource>();
     }
 
-    void musicInfo(string music_name,string musicPath,int score) {
-        jack.texture = Resources.Load<Texture2D>(musicPath);
+    void musicInfo(string music_name,string jacketPath,int score) {
+        audioSource.clip = Resources.Load<AudioClip>(music_name);
+        audioSource.Play();
+        jack.texture = Resources.Load<Texture2D>(jacketPath);
         text1.text = music_name;
     }
 
-    public void Music1()
+    public void Tap(GameObject obj)
+    {
+        Debug.Log(obj.name + "_intro");
+        if(music_number == obj.transform.GetSiblingIndex())
+        {
+            SceneManager.LoadScene("PlayScene");
+        }
+        else
+        {
+            music_number = obj.transform.GetSiblingIndex();
+        }
+        musicInfo("Songs/Music Select/" + obj.name + "_intro", "Jacket/" + obj.name + "_jacket", 1000000);
+    }
+
+    /*public void Music1()
     {
         musicInfo("Collide","Music_Is_My_Suicide_Jacket",1000000);
         audioSource.clip = collide;
@@ -42,5 +61,5 @@ public class MusicNumManage : MonoBehaviour
         SceneManager.LoadScene("PlayScene");
       }
       music_number = 2;
-    }
+    }*/
 }
