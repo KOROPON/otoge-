dedf142343f21d83807e2b03b1cbde1e86a2b335
Outdoor <shoutarou8888ha@gmail.com>
2021-11-14 15:55:57 +0900

    effect

--- Assets/Scripts/Play Scene/Effect/JudgeEffector.cs
+++ Assets/Scripts/Play Scene/Effect/JudgeEffector.cs
@@ -3,24 +3,46 @@ using UnityEngine;
 public class JudgeEffector : MonoBehaviour
 {
     private GameObject[] _effectors;
-    private AudioSource _sePerfect;
-    private AudioSource _seGood;
-    private AudioSource _seBad;
+    private int[] _effectorLanePos;
 
     void Start()
     {
-        _effectors = new GameObject[3];
+        _effectors = new GameObject[10];
+        _effectorLanePos = new int[10];
         var i = 0;
         foreach (Transform child in transform)
         {
             _effectors[i] = child.gameObject;
             i++;
         }
-
     }
 
     public void TapJudgeEffector(int lanePos)
     {
-
+        for (var i =0; i<=2; i++)
+        {
+            var effector = _effectors[i];
+            if (effector.GetComponentsInChildren<Animation>()[0].isPlaying && _effectorLanePos[i] != lanePos)
+            {
+                continue;
+            }
+            //laneposè„Ç©â∫Ç©å©ÇƒEffectorÇÃå¸Ç´çáÇÌÇπÇƒãNìÆ
+            _effectorLanePos.SetValue(lanePos, i);
+            effector.transform.position = RhythmGamePresenter.LanePositions[lanePos];
+            if (lanePos <= 3)//below
+            {
+                effector.GetComponentsInChildren<Animation>()[0].Play();
+                effector.GetComponentsInChildren<Animation>()[1].Play();
+                effector.GetComponentsInChildren<Animation>()[2].Play();
+            }
+            else//above
+            {
+                effector.transform.eulerAngles = new Vector3(0, 0, (16 - lanePos) / 32 * 180);
+                effector.GetComponentsInChildren<Animation>()[0].Play();
+                effector.GetComponentsInChildren<Animation>()[1].Play();
+                effector.GetComponentsInChildren<Animation>()[2].Play();
+            }
+            break;
+        }
     }
 }
