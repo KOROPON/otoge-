ebcc2c402fe7eb7231594a94a0fcdf0454a23ff7
Outdoor <shoutarou8888ha@gmail.com>
2021-12-05 22:17:32 +0900

    tutorial shutter

--- Assets/Scripts/ShutterScene/Shutter.cs
+++ Assets/Scripts/ShutterScene/Shutter.cs
@@ -11,6 +11,7 @@ public class Shutter : MonoBehaviour
     [SerializeField] private GameObject com;
     [SerializeField] private GameObject des;
     [SerializeField] private GameObject dif;
+    [SerializeField] private GameObject pan;
 
     private Animator _anim;
     private static AllJudgeService _judgeService;
@@ -19,6 +20,7 @@ public class Shutter : MonoBehaviour
     private Text _composer;
     private Text _noteDesigner;
     private Text _difficulty;
+    private Image _panel;
     private RhythmGamePresenter _presenter;
 
     public AudioSource openSe;
@@ -38,16 +40,19 @@ public class Shutter : MonoBehaviour
        com.SetActive(true);
         des.SetActive(true);
         dif.SetActive(true);
+        pan.SetActive(true);
         _jacket = jack.GetComponent<Image>();
         _title = name.GetComponent<Text>();
         _composer = com.GetComponent<Text>();
         _noteDesigner = des.GetComponent<Text>();
         _difficulty = dif.GetComponent<Text>();
+        _panel = pan.GetComponent<Image>();
         jack.SetActive(false);
         name.SetActive(false);
         des.SetActive(false);
         com.SetActive(false);
         dif.SetActive(false);
+        pan.SetActive(true);
         _musicM = gameObject.GetComponent<AudioSource>();
         _judgeService = gameObject.AddComponent<AllJudgeService>();
 
@@ -70,6 +75,7 @@ public class Shutter : MonoBehaviour
                 com.SetActive(true);
                 des.SetActive(true);
                 dif.SetActive(true);
+                pan.SetActive(true);
                 var songName = RhythmGamePresenter.musicName;
                 var difficulty = RhythmGamePresenter.dif;
                 _jacket.sprite = Resources.Load<Sprite>("Jacket/" + songName + "_jacket");
@@ -77,12 +83,19 @@ public class Shutter : MonoBehaviour
                 _composer.text = "Composer:" + LevelConverter.GetComposer(songName);
                 _noteDesigner.text = "NoteDesigner:" + LevelConverter.GetNoteEditor(songName, difficulty);
                 _difficulty.text = LevelConverter.GetLevel(songName, difficulty).ToString();
+                _panel.sprite = Resources.Load<Sprite>("Frame/Square");
                 switch (difficulty)
                 {
                     case "Easy": _difficulty.color = new Color32(0, 255, 50, 255);break;
                     case "Hard": _difficulty.color = new Color32(255, 210, 0, 255);break;
                     case "Extreme": _difficulty.color = new Color32(100, 0, 255, 255);break;
                     case "Kujo": _difficulty.color = new Color32(140, 180, 175, 255); break;
+                }
+                if (_title.text == "")
+                {
+                    _title.text = "Tutorial";
+                    _jacket.sprite = Resources.Load<Sprite>("Jacket/IconMain");
+                    _panel.sprite = Resources.Load<Sprite>("Jacket/IconMain");
                 }
                 _anim.SetBool("blToPlay", true);
                 break;
